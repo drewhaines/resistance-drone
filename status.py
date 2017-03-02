9e1b09ef987c0d8dffc2dcfc2750c2f706726867
Drew Haines <drew.dmdevco@gmail.com>
2017-03-02 09:15:09 -0800

    add status file for basic readings and takeoff to 100 meters above see level

---
+++ status.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+© Copyright 2015-2016, 3D Robotics.
+vehicle_state.py:
+
+Demonstrates how to get and set vehicle state and parameter information,
+and how to observe vehicle attribute (state) changes.
+
+Full documentation is provided at http://python.dronekit.io/examples/vehicle_state.html
+"""
+from dronekit import connect, VehicleMode
+import time
+
+#Set up option parsing to get connection string
+import argparse
+parser = argparse.ArgumentParser(description='Print out vehicle state information. Connects to SITL on local PC by default.')
+parser.add_argument('--connect',
+                   help="vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+vehicle = connect("udpin:0.0.0.0:14550", wait_ready=True)
+vehicle.wait_ready('autopilot_version')
+
+# Get all vehicle attributes (state)
+print "\nGet all vehicle attribute values:"
+print " Autopilot Firmware version: %s" % vehicle.version
+print "   Major version number: %s" % vehicle.version.major
+print "   Minor version number: %s" % vehicle.version.minor
+print "   Patch version number: %s" % vehicle.version.patch
+print "   Release type: %s" % vehicle.version.release_type()
+print "   Release version: %s" % vehicle.version.release_version()
+print "   Stable release?: %s" % vehicle.version.is_stable()
+print " Autopilot capabilities"
+print "   Supports MISSION_FLOAT message type: %s" % vehicle.capabilities.mission_float
+print "   Supports PARAM_FLOAT message type: %s" % vehicle.capabilities.param_float
+print "   Supports MISSION_INT message type: %s" % vehicle.capabilities.mission_int
+print "   Supports COMMAND_INT message type: %s" % vehicle.capabilities.command_int
+print "   Supports PARAM_UNION message type: %s" % vehicle.capabilities.param_union
+print "   Supports ftp for file transfers: %s" % vehicle.capabilities.ftp
+print "   Supports commanding attitude offboard: %s" % vehicle.capabilities.set_attitude_target
+print "   Supports commanding position and velocity targets in local NED frame: %s" % vehicle.capabilities.set_attitude_target_local_ned
+print "   Supports set position + velocity targets in global scaled integers: %s" % vehicle.capabilities.set_altitude_target_global_int
+print "   Supports terrain protocol / data handling: %s" % vehicle.capabilities.terrain
+print "   Supports direct actuator control: %s" % vehicle.capabilities.set_actuator_target
+print "   Supports the flight termination command: %s" % vehicle.capabilities.flight_termination
+print "   Supports mission_float message type: %s" % vehicle.capabilities.mission_float
+print "   Supports onboard compass calibration: %s" % vehicle.capabilities.compass_calibration
+print " Global Location: %s" % vehicle.location.global_frame
+print " Global Location (relative altitude): %s" % vehicle.location.global_relative_frame
+print " Local Location: %s" % vehicle.location.local_frame
+print " Attitude: %s" % vehicle.attitude
+print " Velocity: %s" % vehicle.velocity
+print " GPS: %s" % vehicle.gps_0
+print " Gimbal status: %s" % vehicle.gimbal
+print " Battery: %s" % vehicle.battery
+print " EKF OK?: %s" % vehicle.ekf_ok
+print " Last Heartbeat: %s" % vehicle.last_heartbeat
+print " Rangefinder: %s" % vehicle.rangefinder
+print " Rangefinder distance: %s" % vehicle.rangefinder.distance
+print " Rangefinder voltage: %s" % vehicle.rangefinder.voltage
+print " Heading: %s" % vehicle.heading
+print " Is Armable?: %s" % vehicle.is_armable
+print " System status: %s" % vehicle.system_status.state
+print " Groundspeed: %s" % vehicle.groundspeed    # settable
+print " Airspeed: %s" % vehicle.airspeed    # settable
+print " Mode: %s" % vehicle.mode.name    # settable
+print " Armed: %s" % vehicle.armed    # settable
+
+
+
+# Get Vehicle Home location - will be `None` until first set by autopilot
+while not vehicle.home_location:
+    cmds = vehicle.commands
+    cmds.download()
+    cmds.wait_ready()
+    if not vehicle.home_location:
+        print " Waiting for home location ..."
+# We have a home location, so print it!
+print "\n Home location: %s" % vehicle.home_location
+
+
+
+print "\nSet Vehicle.mode = GUIDED (currently: %s)" % vehicle.mode.name
+vehicle.mode = VehicleMode("GUIDED")
+while not vehicle.mode.name=='GUIDED':  #Wait until mode has changed
+    print " Waiting for mode change ..."
+    time.sleep(1)
+
+
+# Check that vehicle is armable
+while not vehicle.is_armable:
+    print " Waiting for vehicle to initialise..."
+    time.sleep(1)
+    # If required, you can provide additional information about initialisation
+    # using `vehicle.gps_0.fix_type` and `vehicle.mode.name`.
+
+print "\nSet Vehicle.armed=True (currently: %s)" % vehicle.armed
+vehicle.armed = True
+while not vehicle.armed:
+    print " Waiting for arming..."
+    time.sleep(1)
+print " Vehicle is armed: %s" % vehicle.armed
+
+
+print "Taking off!"
+vehicle.simple_takeoff(100) # Take off to target altitude
+
+print "Set default/target airspeed to 3"
+vehicle.airspeed = 3
+
+# Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+#  after Vehicle.simple_takeoff will execute immediately).
+while True:
+    print " Altitude: ", vehicle.location.global_relative_frame.alt
+    #Break and return from function just below target altitude.
+    if vehicle.location.global_relative_frame.alt>=99:
+        print "Reached target altitude"
+        break
+    time.sleep(1)
+
+vehicle.mode = VehicleMode("RTL")
+print("Completed")
--- the-resistance-drone.py
+++ the-resistance-drone.py
@@ -13,9 +13,9 @@ import time
 
 
 #Set up option parsing to get connection string
-import argparse  
+import argparse
 parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
-parser.add_argument('--connect', 
+parser.add_argument('--connect',
                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
 
@@ -37,26 +37,26 @@ def arm_and_takeoff(aTargetAltitude):
         print " Waiting for vehicle to initialise..."
         time.sleep(1)
 
-        
+
     print "Arming motors"
     # Copter should arm in GUIDED mode
     vehicle.mode = VehicleMode("GUIDED")
-    vehicle.armed = True    
+    vehicle.armed = True
 
     # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:      
+    while not vehicle.armed:
         print " Waiting for arming..."
         time.sleep(1)
 
     print "Taking off!"
     vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
 
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command 
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
     #  after Vehicle.simple_takeoff will execute immediately).
     while True:
-        print " Altitude: ", vehicle.location.global_relative_frame.alt 
-        #Break and return from function just below target altitude.        
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: 
+        print " Altitude: ", vehicle.location.global_relative_frame.alt
+        #Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
             print "Reached target altitude"
             break
         time.sleep(1)
@@ -97,4 +97,3 @@ vehicle.mode = VehicleMode("RTL")
 #Close vehicle object before exiting script
 print "Close vehicle object"
 vehicle.close()
-
