8c4ebb254181a84ae00112ac1f07c306f0238932
Drew Haines <drew@dmdevco.com>
2017-04-16 09:25:36 -0700

    update gpio test

--- gpio_test.py
+++ gpio_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import RPi.GPIO as GPIO
 import time
 
@@ -12,18 +13,25 @@ GPIO.output(27, GPIO.LOW)
 print("\n Drop GPIO 1!")
 GPIO.output(17, GPIO.LOW)
 GPIO.output(27, GPIO.HIGH)
+print("\n GPIO 17 %3d" % GPIO.input(17))
+print("\n GPIO 17 %3d" % GPIO.input(27))
+
 time.sleep(3)
 
 # drop payload by toggling GPIO pins
 print("\n Drop GPIO 2!")
 GPIO.output(17, GPIO.HIGH)
 GPIO.output(27, GPIO.LOW)
+print("\n GPIO 17 %3d" % GPIO.input(17))
+print("\n GPIO 17 %3d" % GPIO.input(27))
 time.sleep(3)
 
 # drop payload by toggling GPIO pins
 print("\n Drop GPIO 3!")
 GPIO.output(17, GPIO.HIGH)
 GPIO.output(27, GPIO.HIGH)
+print("\n GPIO 17 %3d" % GPIO.input(17))
+print("\n GPIO 17 %3d" % GPIO.input(27))
 time.sleep(3)
 
 
