bd4629a3452359149de4beea0637f9667405bcac
Drew Haines <drew@dmdevco.com>
2017-04-16 10:27:35 -0700

    gpio again 2

--- gpio_test.py
+++ gpio_test.py
@@ -12,8 +12,8 @@ GPIO.output(27, GPIO.LOW)
 while 1:
     # drop payload by toggling GPIO pins
     print("\n Drop GPIO!")
-    GPIO.output(17, GPIO.HIGH)
-    GPIO.output(27, GPIO.HIGH)
+    GPIO.output(17, 1)
+    GPIO.output(27, 1)
     print("\n GPIO 17 %3d" % GPIO.input(17))
     print("\n GPIO 27 %3d" % GPIO.input(27))
     time.sleep(3)
