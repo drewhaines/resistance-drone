bd8845de08e9c9423e317e1847d3e7b54448188f
Drew Haines <drew@dmdevco.com>
2017-04-16 10:11:09 -0700

    gpio again

--- gpio_test.py
+++ gpio_test.py
@@ -9,30 +9,14 @@ GPIO.setup(27, GPIO.OUT)
 GPIO.output(17, GPIO.LOW)
 GPIO.output(27, GPIO.LOW)
 
-# drop payload by toggling GPIO pins
-print("\n Drop GPIO 1!")
-GPIO.output(17, GPIO.LOW)
-GPIO.output(27, GPIO.HIGH)
-print("\n GPIO 17 %3d" % GPIO.input(17))
-print("\n GPIO 27 %3d" % GPIO.input(27))
-
-time.sleep(3)
-
-# drop payload by toggling GPIO pins
-print("\n Drop GPIO 2!")
-GPIO.output(17, GPIO.HIGH)
-GPIO.output(27, GPIO.LOW)
-print("\n GPIO 17 %3d" % GPIO.input(17))
-print("\n GPIO 27 %3d" % GPIO.input(27))
-time.sleep(3)
-
-# drop payload by toggling GPIO pins
-print("\n Drop GPIO 3!")
-GPIO.output(17, GPIO.HIGH)
-GPIO.output(27, GPIO.HIGH)
-print("\n GPIO 17 %3d" % GPIO.input(17))
-print("\n GPIO 27 %3d" % GPIO.input(27))
-time.sleep(3)
+while 1:
+    # drop payload by toggling GPIO pins
+    print("\n Drop GPIO!")
+    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(27, GPIO.HIGH)
+    print("\n GPIO 17 %3d" % GPIO.input(17))
+    print("\n GPIO 27 %3d" % GPIO.input(27))
+    time.sleep(3)
 
 
 print("Completed")
