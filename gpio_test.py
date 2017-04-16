e47b5ccb2af35b1470d3e58714674044143aba69
Drew Haines <drew@dmdevco.com>
2017-04-16 10:56:46 -0700

    gpio again 3

--- gpio_test.py
+++ gpio_test.py
@@ -3,19 +3,19 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(27, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
 
-GPIO.output(17, GPIO.LOW)
-GPIO.output(27, GPIO.LOW)
+GPIO.output(13, GPIO.LOW)
+GPIO.output(19, GPIO.LOW)
 
 while 1:
     # drop payload by toggling GPIO pins
     print("\n Drop GPIO!")
-    GPIO.output(17, 1)
-    GPIO.output(27, 1)
-    print("\n GPIO 17 %3d" % GPIO.input(17))
-    print("\n GPIO 27 %3d" % GPIO.input(27))
+    GPIO.output(13, 1)
+    GPIO.output(19, 1)
+    print("\n GPIO 17 %3d" % GPIO.input(13))
+    print("\n GPIO 27 %3d" % GPIO.input(19))
     time.sleep(3)
 
 
